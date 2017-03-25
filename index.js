7864d8b0977034cc4c2659a2637940aacc5575a2
Nathan <nathanbommezijn@gmail.com>
2017-03-25 23:02:17 +0100

    added firebaseCredit to gitignore

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
 const $http = require('request-promise');
+const firebase = require('firebase-admin');
+
+var serviceAccount = require("./firebaseCredit.json");
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://samsam-431d1.firebaseio.com"
+});
 
 // middleware
 var port = process.env.PORT || 8080;
@@ -14,19 +22,20 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
+
+    
     res.send(200);
 })
 
 app.get("/product/:id", (req, res) => {
-    res.json({
-        price: 500,
-        image: 'someimage',
-        title: "rains shirt"
+
+    firebase.database().ref(`/${req.params.id}`).once('value').then( data => {
+       res.json(data.val())
     });
 });
 
-app.post("/checkout", (req, res) => {
-    
+app.post("/product", (req, res) => {
+    firebase.database().ref('products/').push(req.body.product);
 });
 
 //use port variable for deployment
